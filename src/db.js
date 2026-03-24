8e4b8cd709c921c5021885f6da7983d984397d1f
krishnakasaudhan0 <kasaudhankrishna05@gmail.com>
2026-03-24 04:12:18 +0000

    refactor: clean database connection pool settings

--- src/db.js
+++ src/db.js
@@ -2,3 +2,5 @@
 // Git contribution logic update at: 2026-03-23T04:04:13.000Z [hash: uzpgwq]
 
 // Git contribution logic update at: 2026-03-23T05:46:35.000Z [hash: wq8p0c]
+
+// Git contribution logic update at: 2026-03-24T04:12:18.000Z [hash: loq8eq]
