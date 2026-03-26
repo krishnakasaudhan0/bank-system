049576d67b6c21782dd45fcd3748b76043676685
krishnakasaudhan0 <kasaudhankrishna05@gmail.com>
2026-03-26 10:27:13 +0000

    fix: prevent race condition on parallel balance withdrawal

--- src/accounts.js
+++ src/accounts.js
@@ -2,3 +2,5 @@
 // Git contribution logic update at: 2026-03-22T05:55:57.000Z [hash: enae49]
 
 // Git contribution logic update at: 2026-03-26T13:14:38.000Z [hash: 6sd07l]
+
+// Git contribution logic update at: 2026-03-26T10:27:13.000Z [hash: sr1jna]
